8f9aa164f3839ab0619bc44597fc68823853d75b
johnngugi <johnngugi777@gmail.com>
2021-09-17 10:53:32 +0300

    chore: remove unused strings and assets

--- lib/domain/core/value_objects/asset_strings.dart
+++ lib/domain/core/value_objects/asset_strings.dart
@@ -3,29 +3,13 @@ const String landingBackgroundImg = 'assets/images/landing.jpg';
 
 const String closeIconUrl = 'assets/icons/close.png';
 const String errorIconUrl = 'assets/icons/error.svg';
-const String warningIconUrl = 'assets/icons/warning.svg';
 const String infoIconUrl = 'assets/icons/info.svg';
-const String successIconUrl = 'assets/icons/success.svg';
-
-const String authorizationIcon = 'assets/images/authorization_icon.svg';
 
 // the infamous spaceman used in the main files
 const String errorDisplayImgUrl = 'assets/images/error-display.png';
 
-const String spaceImageUrl = 'assets/images/space.png';
-
-const String moneyIconImageUrl = 'assets/images/money.png';
-
-const String singleSpentAmountBackgroundImgUrl =
-    'assets/images/cardseffect.png';
-
 //  search page icons
-const String wellnessSearchImgUrl = 'assets/images/wellness_search.svg';
-const String shieldWellnessImgUrl = 'assets/images/shield_wellness.svg';
-const String coachImgUrl = 'assets/images/wellness_search.svg';
-const String cartSearchImgUrl = 'assets/images/cart_search.svg';
 const String libraryImgUrl = 'assets/images/book.svg';
-const String coversSearchImgUrl = 'assets/images/covers_search.svg';
 const String medicationsSearchImgUrl = 'assets/icons/meds_search.svg';
 const String testSearchImgUrl = 'assets/icons/tests_search.svg';
 const String doctorSearchImgUrl = 'assets/icons/doctor_search.svg';
@@ -36,127 +20,30 @@ const String blendedBackgroundImg = 'assets/images/mountain_bg.jpg';
 
 // bottom navigation bar icon urls
 const String homeNavItemIconUrl = 'assets/images/home.svg';
-const String coachNavItemIconUrl = 'assets/images/coach.svg';
 const String libraryNavItemIconUrl = 'assets/images/library.svg';
-const String cartNavItemIconUrl = 'assets/images/cart.svg';
-const String coversNavItemIconUrl = 'assets/images/covers.svg';
-
-const String insuranceContentGymImgUrl = 'assets/images/Gymnist lady.png';
-
-const String helpCenterCareImgUrl = 'assets/images/care.svg';
-
-const String landingPageImgUrl = 'assets/images/landing.svg';
 
 const String shieldIconImgUrl = 'assets/images/shield.svg';
-const String communicationAlertStarsImgUrl = 'assets/images/stars.svg';
 
 const String wellnessUmbrellaImgUrl = 'assets/images/wellness2.jpeg';
 const String wellnessBackgroundImgUrl = 'assets/images/wellness.jpg';
 
-const String defaultLogoImageURL = 'assets/images/white_logo.png';
-
-const String coachFitnessImgUrl = 'assets/images/coach_fitness.svg';
-
-const String shoppingCartImgUrl = 'assets/images/shopping_cart.jpg';
-
-const String emptyCartIconUrl = 'assets/icons/empty_cart.svg';
-
-const String cartTrashIconUrl = 'assets/icons/trash.png';
-
 const String comingSoonImgUrl = 'assets/images/coming_soon.jpg';
-
-const String walkIllustrationImgUrl = 'assets/images/illustration_walk.png';
-
-const String customerCareImagePath = 'assets/images/care.png';
 
 // help center button strings
 const String helpCenterButtonImageUrl = 'assets/images/support_icon.svg';
-
-const String trophyGreenSVG = 'assets/images/trophy_green.svg';
-const String trophyPurpleSVG = 'assets/images/trophy_purple.svg';
-const String wellnessPayPageImageUrl = 'assets/images/qr_code_placeholder.png';
-
-const String noCoversImgUrl = 'assets/images/empty.png';
 
 const String doctorLandingPageImgUrl = 'assets/images/landingdoc.svg';
 
 const String laboratoryCoverIconImgUrl = 'assets/images/laboratory.jpg';
 
-const String noVirtualCardImgUrl = 'assets/images/no_virtual_card.png';
-
-const String nhifLogoImgUrl = 'assets/images/nhif-logo.png';
-
 const String noConnectionImgUrl = 'assets/images/wifi.png';
 
 const String noDataImgUrl = 'assets/images/no_data.svg';
 
-const String medicationCoverIconUrl = 'assets/icons/empty_page.svg';
-
 const String genericNoDataIconUrl = 'assets/icons/not_found.svg';
 
-const String marketplaceIllustrationPath = 'assets/images/doctor.png';
-const String marketplaceJubileeImgPath = 'assets/images/jubilee.png';
-const String marketplaceApaImgPath = 'assets/images/apa.jpg';
-const String marketplaceMadisonImgPath = 'assets/images/madison.png';
-const String marketplaceBritamImgPath = 'assets/images/britam.png';
-const String marketplaceUAPImgPath = 'assets/images/uap.png';
-
-const String noSignalSvg = 'assets/images/no_signal.svg';
-const String noWifiSvg = 'assets/images/no_wifi.svg';
-
-const String medicalCareSvg = 'assets/images/medicalcare.svg';
-const String searchingDataSVG = 'assets/images/searching-data.svg';
-
-
-const String marketplaceBackgroundBannerImagePath =
-    'assets/images/market_bg.svg';
-const String marketplaceDiscountImagePath = 'assets/images/discount.png';
-const String marketplaceTempBgImagePath = 'assets/images/action_banner.jpg';
-
-const String opticalAddBenefitsSvg = 'assets/images/optical.svg';
-const String dentalAddBenefitsSvg = 'assets/images/dental.svg';
-const String maternityAddBenefitsSvg = 'assets/images/maternity.svg';
-const String jubileeTransparentLogo = 'assets/images/jubilee.png';
-const String jubileeBackgroundLogo = 'assets/images/jubilee_back_logo.png';
-const String apaTransparentLogo = 'assets/images/apa_logo.png';
-const String apaBackgroundLogo = 'assets/images/apa_back_logo.png';
-const String madisonTransparentLogo = 'assets/images/madison_logo.png';
-const String madisonBackgroundLogo = 'assets/images/madison_back_logo.png';
-const String resolutionTransparentLogo = 'assets/images/resolution_logo.png';
-const String resolutionBackgroundLogo =
-    'assets/images/resolution_back_logo.png';
-const String britamTransparentLogo = 'assets/images/britam_logo.png';
-const String britamBackgroundLogo = 'assets/images/britam_back_logo.png';
-const String cornerCircle = 'assets/images/corner_circle.png';
-const String coverStatusNudgePngImage = 'assets/images/side_img.png';
-const String paymentSVGPath = 'assets/images/pay.svg';
-const String underwritingSVGPath = 'assets/images/underwriting.svg';
-const String selectSVGPath = 'assets/images/select.svg';
 const String slideOneImg = 'assets/images/find_your_cover.svg';
 const String slideTwoImg = 'assets/images/add_your_card.svg';
 const String slideThreeImg = 'assets/images/track_your_expenses.svg';
 const String slideFourImg = 'assets/images/view_your_benefits.svg';
 const String slideFiveImg = 'assets/images/build_health_habits.svg';
-
-const String buyMedicalCoverSvg = 'assets/images/buy_medical_cover.svg';
-const String buyHRASvg = 'assets/images/resume_hra.svg';
-const String buyCaptureDependantsSvg =
-    'assets/images/resume_capture_dependants_info.svg';
-const String buyPaymentSvg = 'assets/images/resume_payment.svg';
-const String buyKYCSvg = 'assets/images/resume_capture_kyc.svg';
-const String buySelectCoverSvg = 'assets/images/resume_select_cover.svg';
-const String buySelectRangeSvg = 'assets/images/resume_select_range.svg';
-
-const String forMyselfIconsSvg = 'assets/images/for_myself_icon.svg';
-const String forMyselfNFamilyIconsSvg =
-    'assets/images/for_myself_n_family_icon.svg';
-const String forSomeoneElseIconSvg = 'assets/images/for_someone_else.svg';
-
-const String awesomeSVG = 'assets/images/awesome.svg';
-const String bevSVG = 'assets/images/bev.svg';
-const String bevConfirmationFailedSVG =
-    'assets/images/bev_confirmation_failed.svg';
-const String bevPhoneSVG = 'assets/images/bev_phone.svg';
-const String bevPieceSignSVG = 'assets/images/bev_piece_sign.svg';
-const String bevPlantSVG = 'assets/images/bev_plant.svg';
-const String bevSuccessSVG = 'assets/images/bev_success.svg';

601ad9b6ac880cfe75ce333a6c64c53ab3ab61a2
Paul Mburu <paulmburu53@gmail.com>
2021-09-27 12:36:36 +0300

    chore: add primary button

---
+++ test/widgets_tests/presentation/core/widgets/buttons_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:myafyahub/presentation/core/widgets/my_afya_hub_primary_button.dart';
+
+void main() {
+  group('SILPrimaryButton', () {
+    testWidgets('should render correctly', (WidgetTester tester) async {
+      bool isActionTapped = false;
+      bool isLongPressed = false;
+
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Builder(builder: (BuildContext context) {
+              return MyAfyaHubPrimaryButton(onPressed: () {
+                isActionTapped = true;
+              }, onLongPress: () {
+                isLongPressed = true;
+              });
+            }),
+          ),
+        ),
+      ));
+
+      expect(find.byType(RawMaterialButton), findsOneWidget);
+
+      await tester.tap(find.byType(MyAfyaHubPrimaryButton));
+      await tester.pumpAndSettle();
+      expect(isActionTapped, true);
+
+      await tester.longPress(find.byType(MyAfyaHubPrimaryButton));
+      await tester.pumpAndSettle();
+      expect(isLongPressed, true);
+    });
+  });
+}

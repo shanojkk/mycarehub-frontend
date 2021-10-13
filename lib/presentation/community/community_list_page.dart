2e1b38f8b009e32fc52b8297d900effdfe36c38b
Abiud <abiudrn@gmail.com>
2021-10-13 21:28:59 +0300

    chore: swap out community list item

--- lib/presentation/community/community_list_page.dart
+++ lib/presentation/community/community_list_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 // Project imports:
 import 'package:myafyahub/domain/core/value_objects/app_strings.dart';
 import 'package:myafyahub/domain/core/value_objects/app_widget_keys.dart';
-import 'package:myafyahub/presentation/community/community_list_item.dart';
+import 'package:afya_moja_core/community_list_item.dart';
 import 'package:myafyahub/presentation/core/widgets/app_bar/custom_app_bar.dart';
 
 class CommunityListViewPage extends StatelessWidget {
@@ -13,7 +13,7 @@ class CommunityListViewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Placeholder
-    const String dontMissAppointmentMessage =
+    const String appointmentReminderText =
         'Don\'t miss your appointment tomorrow';
     const AssetImage avatarImage = AssetImage('assets/images/wellness2.jpeg');
     const String date = 'July 12 2021';
@@ -21,76 +21,35 @@ class CommunityListViewPage extends StatelessWidget {
     const List<Widget> items = <Widget>[
       CommunityListItem(
         title: 'Ruaka Questions group',
+        message: appointmentReminderText,
+        lastMessageDate: date,
         unreadNotificationCount: 4,
-        isGroup: true,
-        message: dontMissAppointmentMessage,
         avatarImage: avatarImage,
-        date: date,
+        isGroupMessage: true,
       ),
       CommunityListItem(
         title: 'Ruaka Questions group',
-        isGroup: true,
-        message: dontMissAppointmentMessage,
+        message: appointmentReminderText,
+        lastMessageDate: date,
+        unreadNotificationCount: 4,
         avatarImage: avatarImage,
-        date: date,
+        isGroupMessage: true,
       ),
       CommunityListItem(
         title: 'My Afya Guide',
-        message: dontMissAppointmentMessage,
+        message: appointmentReminderText,
+        lastMessageDate: date,
+        unreadNotificationCount: 4,
         avatarImage: avatarImage,
-        date: date,
+        isGroupMessage: true,
       ),
       CommunityListItem(
         title: 'My Afya Guide',
+        message: appointmentReminderText,
+        lastMessageDate: date,
         unreadNotificationCount: 4,
-        message: dontMissAppointmentMessage,
         avatarImage: avatarImage,
-        date: date,
-      ),
-      CommunityListItem(
-        title: 'Ruaka Questions group',
-        isGroup: true,
-        message: dontMissAppointmentMessage,
-        avatarImage: avatarImage,
-        date: date,
-      ),
-      CommunityListItem(
-        title: 'My Afya Guide',
-        message: dontMissAppointmentMessage,
-        avatarImage: avatarImage,
-        date: date,
-      ),
-      CommunityListItem(
-        title: 'Ruaka Questions group',
-        unreadNotificationCount: 4,
-        isGroup: true,
-        message: dontMissAppointmentMessage,
-        avatarImage: avatarImage,
-        date: date,
-      ),
-      CommunityListItem(
-        title: 'Ruaka Questions group',
-        unreadNotificationCount: 4,
-        isGroup: true,
-        message: dontMissAppointmentMessage,
-        avatarImage: avatarImage,
-        date: date,
-      ),
-      CommunityListItem(
-        title: 'Ruaka Questions group',
-        unreadNotificationCount: 4,
-        isGroup: true,
-        message: dontMissAppointmentMessage,
-        avatarImage: avatarImage,
-        date: date,
-      ),
-      CommunityListItem(
-        title: 'Ruaka Questions group',
-        unreadNotificationCount: 4,
-        isGroup: true,
-        message: dontMissAppointmentMessage,
-        avatarImage: avatarImage,
-        date: date,
+        isGroupMessage: true,
       ),
     ];
 

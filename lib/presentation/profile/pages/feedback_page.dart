ef545e6c16377542cfe735a4cebca9099ae4bd29
KellyEgesa <kelly.egesa@gmail.com>
2021-10-19 14:54:26 +0300

    fix: feedback page

--- lib/presentation/profile/pages/feedback_page.dart
+++ lib/presentation/profile/pages/feedback_page.dart
@@ -1,14 +1,18 @@
+// Flutter imports
 import 'package:flutter/material.dart';
-import 'package:misc_utilities/misc.dart';
+
+// Package imports
+import 'package:afya_moja_core/buttons.dart';
+import 'package:shared_themes/spaces.dart';
+import 'package:shared_themes/text_themes.dart';
+
+// Project imports
 import 'package:myafyahub/domain/core/value_objects/app_strings.dart';
 import 'package:myafyahub/domain/core/value_objects/app_widget_keys.dart';
 import 'package:myafyahub/domain/core/value_objects/enums.dart';
 import 'package:myafyahub/presentation/core/theme/theme.dart';
 import 'package:myafyahub/presentation/core/widgets/app_bar/custom_app_bar.dart';
-import 'package:afya_moja_core/buttons.dart';
 import 'package:myafyahub/presentation/profile/widgets/medical_data_item_title.dart';
-import 'package:shared_themes/spaces.dart';
-import 'package:shared_themes/text_themes.dart';
 
 class FeedbackPage extends StatefulWidget {
   /// [FeedbackPage] is used to get user feedback on app usage
@@ -21,111 +25,121 @@ class FeedbackPage extends StatefulWidget {
 
 class _FeedbackPageState extends State<FeedbackPage> {
   late FollowUpChoice followUpChoice = FollowUpChoice.Unknown;
+  final TextEditingController feedBackInputController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
       appBar: const CustomAppBar(title: feedbackString),
-      body: SizedBox(
-        height: MediaQuery.of(context).size.height,
+      body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Stack(
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height / 1.2,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Stack(
                 children: <Widget>[
-                  largeVerticalSizedBox,
-                  const MedicalDataItemTitle(title: weValueFeedbackString),
-                  Text(
-                    howToImproveExperienceString,
-                    style: TextThemes.lightSize16Text(AppColors.greyTextColor)
-                        .copyWith(fontSize: 12),
-                  ),
-                  veryLargeVerticalSizedBox,
-                  const MedicalDataItemTitle(title: anyThoughtsToShareString),
-                  const TextField(
-                    keyboardType: TextInputType.multiline,
-                    maxLines: 9,
-                    minLines: 8,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        borderSide: BorderSide(color: AppColors.secondaryColor),
-                      ),
-                    ),
-                  ),
-                  veryLargeVerticalSizedBox,
-                  const MedicalDataItemTitle(title: followUpOnFeedbackString),
-                  Row(
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Row(
-                        children: <Widget>[
-                          SizedBox(
-                            width: 20,
-                            height: 20,
-                            child: Radio<FollowUpChoice>(
-                              key: yesRadioButtonKey,
-                              activeColor: AppColors.secondaryColor,
-                              value: FollowUpChoice.Yes,
-                              groupValue: followUpChoice,
-                              onChanged: (FollowUpChoice? value) {
-                                setState(() {
-                                  followUpChoice = value!;
-                                });
-                              },
-                            ),
-                          ),
-                          verySmallHorizontalSizedBox,
-                          Text(
-                            yesString,
-                            style: TextThemes.lightSize16Text(
-                                    AppColors.greyTextColor)
-                                .copyWith(
-                              fontSize: 12,
-                            ),
-                          )
-                        ],
+                      largeVerticalSizedBox,
+                      const MedicalDataItemTitle(title: weValueFeedbackString),
+                      Text(
+                        howToImproveExperienceString,
+                        style:
+                            TextThemes.lightSize16Text(AppColors.greyTextColor)
+                                .copyWith(fontSize: 12),
                       ),
-                      veryLargeHorizontalSizedBox,
-                      smallHorizontalSizedBox,
+                      veryLargeVerticalSizedBox,
+                      const MedicalDataItemTitle(
+                          title: anyThoughtsToShareString),
+                      TextField(
+                        controller: feedBackInputController,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: 9,
+                        minLines: 8,
+                        decoration: const InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                            borderSide:
+                                BorderSide(color: AppColors.secondaryColor),
+                          ),
+                        ),
+                      ),
+                      veryLargeVerticalSizedBox,
+                      const MedicalDataItemTitle(
+                          title: followUpOnFeedbackString),
                       Row(
                         children: <Widget>[
-                          SizedBox(
-                            width: 20,
-                            child: Radio<FollowUpChoice>(
-                              key: noRadioButtonKey,
-                              value: FollowUpChoice.No,
-                              groupValue: followUpChoice,
-                              onChanged: (FollowUpChoice? value) {
-                                setState(() {
-                                  followUpChoice = value!;
-                                });
-                              },
-                              activeColor: AppColors.secondaryColor,
-                            ),
+                          Row(
+                            children: <Widget>[
+                              SizedBox(
+                                width: 20,
+                                height: 20,
+                                child: Radio<FollowUpChoice>(
+                                  key: yesRadioButtonKey,
+                                  activeColor: AppColors.secondaryColor,
+                                  value: FollowUpChoice.Yes,
+                                  groupValue: followUpChoice,
+                                  onChanged: (FollowUpChoice? value) {
+                                    setState(() {
+                                      followUpChoice = value!;
+                                    });
+                                  },
+                                ),
+                              ),
+                              verySmallHorizontalSizedBox,
+                              Text(
+                                yesString,
+                                style: TextThemes.lightSize16Text(
+                                        AppColors.greyTextColor)
+                                    .copyWith(
+                                  fontSize: 12,
+                                ),
+                              )
+                            ],
                           ),
-                          verySmallHorizontalSizedBox,
-                          Text(
-                            noString,
-                            style: TextThemes.lightSize16Text(
-                                    AppColors.greyTextColor)
-                                .copyWith(
-                              fontSize: 12,
-                            ),
-                          )
+                          veryLargeHorizontalSizedBox,
+                          smallHorizontalSizedBox,
+                          Row(
+                            children: <Widget>[
+                              SizedBox(
+                                width: 20,
+                                child: Radio<FollowUpChoice>(
+                                  key: noRadioButtonKey,
+                                  value: FollowUpChoice.No,
+                                  groupValue: followUpChoice,
+                                  onChanged: (FollowUpChoice? value) {
+                                    setState(() {
+                                      followUpChoice = value!;
+                                    });
+                                  },
+                                  activeColor: AppColors.secondaryColor,
+                                ),
+                              ),
+                              verySmallHorizontalSizedBox,
+                              Text(
+                                noString,
+                                style: TextThemes.lightSize16Text(
+                                        AppColors.greyTextColor)
+                                    .copyWith(
+                                  fontSize: 12,
+                                ),
+                              )
+                            ],
+                          ),
                         ],
                       ),
                     ],
                   ),
-                ],
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Row(
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         SizedBox(
@@ -134,10 +148,11 @@ class _FeedbackPageState extends State<FeedbackPage> {
                           child: MyAfyaHubPrimaryButton(
                             buttonKey: sendFeedbackButtonKey,
                             onPressed: () {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  snackbar(content: comingSoonText));
+                              Navigator.of(context).pop();
                             },
-                            buttonColor: AppColors.secondaryColor,
+                            buttonColor: feedBackInputController.text.isNotEmpty
+                                ? AppColors.secondaryColor
+                                : Colors.grey,
                             borderColor: Colors.transparent,
                             text: sendFeedbackString,
                             textStyle:
@@ -149,6 +164,9 @@ class _FeedbackPageState extends State<FeedbackPage> {
                           width: 150,
                           height: 40,
                           child: MyAfyaHubPrimaryButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
                             buttonKey: cancelFeedbackButtonKey,
                             buttonColor: Colors.transparent,
                             borderColor: AppColors.secondaryColor,
@@ -157,13 +175,13 @@ class _FeedbackPageState extends State<FeedbackPage> {
                                 AppColors.secondaryColor),
                           ),
                         ),
+                        largeVerticalSizedBox,
                       ],
                     ),
-                    largeVerticalSizedBox,
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
--- test/widgets_tests/presentation/profile/pages/feedback_page_test.dart
+++ test/widgets_tests/presentation/profile/pages/feedback_page_test.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:myafyahub/domain/core/value_objects/app_strings.dart';
 import 'package:myafyahub/domain/core/value_objects/app_widget_keys.dart';
-import 'package:myafyahub/presentation/core/widgets/app_bar/custom_app_bar.dart';
+import 'package:myafyahub/domain/core/value_objects/enums.dart';
 import 'package:myafyahub/presentation/profile/pages/feedback_page.dart';
 
 void main() {
@@ -19,28 +18,86 @@ void main() {
         ),
       ),
     );
-    expect(find.byType(CustomAppBar), findsWidgets);
+    expect(find.byType(FeedbackPage), findsOneWidget);
+    expect(find.byType(TextField), findsOneWidget);
+  });
+
+  testWidgets('FeedbackPage pop when sendFeedback is clicked',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Builder(
+            builder: (BuildContext context) {
+              return const FeedbackPage();
+            },
+          ),
+        ),
+      ),
+    );
 
     final Finder sendRequestButton = find.byKey(sendFeedbackButtonKey);
+
+    await tester.ensureVisible(sendRequestButton);
+    await tester.tap(sendRequestButton);
+    await tester.pumpAndSettle();
+    expect(find.byType(FeedbackPage), findsNothing);
+  });
+
+  testWidgets('FeedbackPage pop when cancel is clicked',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Builder(
+            builder: (BuildContext context) {
+              return const FeedbackPage();
+            },
+          ),
+        ),
+      ),
+    );
+
     final Finder cancelRequestButton = find.byKey(cancelFeedbackButtonKey);
+
+    expect(cancelRequestButton, findsOneWidget);
+
+    await tester.ensureVisible(cancelRequestButton);
+    await tester.tap(cancelRequestButton);
+    await tester.pumpAndSettle();
+    expect(find.byType(FeedbackPage), findsNothing);
+  });
+
+  testWidgets('FeedbackPage radio buttons are clickable ',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Builder(
+            builder: (BuildContext context) {
+              return const FeedbackPage();
+            },
+          ),
+        ),
+      ),
+    );
+
     final Finder yesRadioButton = find.byKey(yesRadioButtonKey);
     final Finder noRadioButton = find.byKey(noRadioButtonKey);
-    expect(sendRequestButton, findsOneWidget);
-    expect(cancelRequestButton, findsOneWidget);
+
     expect(yesRadioButton, findsOneWidget);
     expect(noRadioButton, findsOneWidget);
 
-    await tester.ensureVisible(sendRequestButton);
     await tester.tap(yesRadioButton);
-    await tester.tap(sendRequestButton);
-    await tester.pumpAndSettle(const Duration(seconds: 1));
-    expect(find.text(comingSoonText), findsOneWidget);
-    await tester.pumpAndSettle(const Duration(seconds: 2));
+    await tester.pumpAndSettle();
 
-    await tester.ensureVisible(cancelRequestButton);
+    expect((tester.firstWidget(yesRadioButton) as Radio<FollowUpChoice>).value,
+        FollowUpChoice.Yes);
+
     await tester.tap(noRadioButton);
-    await tester.tap(cancelRequestButton);
-    await tester.pumpAndSettle(const Duration(seconds: 1));
-    expect(find.text(comingSoonText), findsOneWidget);
+    await tester.pumpAndSettle();
+
+    expect((tester.firstWidget(noRadioButton) as Radio<FollowUpChoice>).value,
+        FollowUpChoice.No);
   });
 }

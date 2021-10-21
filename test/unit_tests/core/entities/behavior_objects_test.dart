50ca6087f55e5f89c4b927eb683f9f936f55196d
Abiud Orina <abiudrn@gmail.com>
2021-10-21 14:37:05 +0300

    chore: remove unused code

--- test/unit_tests/core/entities/behavior_objects_test.dart
+++ test/unit_tests/core/entities/behavior_objects_test.dart
@@ -2,34 +2,11 @@
 import 'package:flutter_test/flutter_test.dart';
 
 // Project imports:
-import 'package:myafyahub/domain/core/entities/core/behavior_objects.dart';
 import 'package:myafyahub/domain/core/entities/core/event_obj.dart';
 import 'package:myafyahub/domain/core/entities/login/onboarding_behavior_subject.dart';
 import 'package:myafyahub/domain/core/value_objects/app_strings.dart';
 
 void main() {
-  group('StartVisitIdentificationOTP', () {
-    test('should return singleton ', () {
-      final StartVisitIdentificationOTP obj1 = StartVisitIdentificationOTP();
-      final StartVisitIdentificationOTP obj2 = StartVisitIdentificationOTP();
-
-      expect(obj1, equals(obj2));
-      expect(
-        obj1.codeHasTakenLong.valueOrNull,
-        obj2.codeHasTakenLong.valueOrNull,
-      );
-
-      obj1.codeHasTakenLong.add(true);
-
-      expect(obj1.codeHasTakenLong.valueOrNull, true);
-
-      expect(
-        obj1.codeHasTakenLong.valueOrNull,
-        obj2.codeHasTakenLong.valueOrNull,
-      );
-    });
-  });
-
   group('SessionUserType', () {
     test('should return singleton ', () {
       final SessionUserType obj1 = SessionUserType();

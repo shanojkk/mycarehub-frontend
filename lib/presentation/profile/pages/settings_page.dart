a83b6154560e7d784d9c5cebb042e507719f97ec
Eugene <eugenemrlens@gmail.com>
2021-11-09 16:10:25 +0300

    fix: padding and edit button on settings page

--- lib/presentation/profile/pages/settings_page.dart
+++ lib/presentation/profile/pages/settings_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:myafyahub/domain/core/value_objects/app_strings.dart';
+import 'package:myafyahub/presentation/core/theme/theme.dart';
 import 'package:myafyahub/presentation/core/widgets/app_bar/custom_app_bar.dart';
-import 'package:myafyahub/presentation/profile/widgets/medical_data_item.dart';
-import 'package:myafyahub/presentation/profile/widgets/medical_data_item_title.dart';
+import 'package:myafyahub/presentation/core/widgets/personal_information_widget.dart';
 import 'package:shared_themes/spaces.dart';
+import 'package:shared_themes/text_themes.dart';
 
 /// [SettingsPage] is used to display the client's settings
 ///
@@ -14,13 +15,13 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
       appBar: const CustomAppBar(title: settingsPageTitle),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Container(
+          child: SizedBox(
             height: double.infinity,
-            color: Theme.of(context).backgroundColor,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -29,11 +30,33 @@ class SettingsPage extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
                     largeVerticalSizedBox,
-                    const MedicalDataItemTitle(title: nickNameString),
-                    MedicalDataItem(
-                      customWidth: double.infinity,
-                      data: userNickName,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          nickNameString,
+                          style: TextThemes.boldSize15Text(
+                              AppColors.secondaryColor),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 4),
+                          decoration: BoxDecoration(
+                            color: AppColors.userInitialsColor,
+                            borderRadius: BorderRadius.circular(7),
+                          ),
+                          child: Text(
+                            editString,
+                            textAlign: TextAlign.center,
+                            style: TextThemes.normalSize14Text(
+                                AppColors.whiteColor),
+                          ),
+                        ),
+                      ],
                     ),
+                    smallVerticalSizedBox,
+                    PersonalInformationWidget(description: userNickName),
                   ],
                 ),
               ],
--- test/widgets_tests/presentation/profile/settings_page_test.dart
+++ test/widgets_tests/presentation/profile/settings_page_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:myafyahub/domain/core/value_objects/app_strings.dart';
 import 'package:myafyahub/presentation/core/widgets/app_bar/custom_app_bar.dart';
 import 'package:myafyahub/presentation/profile/pages/settings_page.dart';
 
@@ -9,6 +10,6 @@ void main() {
     await tester.pumpWidget(MaterialApp(home: SettingsPage()));
 
     expect(find.byType(CustomAppBar), findsOneWidget);
-    expect(find.text('John Doe'), findsOneWidget);
+    expect(find.text(editString), findsOneWidget);
   });
 }

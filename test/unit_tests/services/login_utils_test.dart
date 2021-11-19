88756f60eae2482bd8a2b6e269e3e70767166071
KellyEgesa <kelly.egesa@gmail.com>
2021-11-19 17:12:53 +0300

    fix: login utils test coverage

--- test/unit_tests/services/login_utils_test.dart
+++ test/unit_tests/services/login_utils_test.dart
@@ -1,14 +1,19 @@
 // Flutter imports:
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 // Package imports:
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:mocktail_image_network/mocktail_image_network.dart';
 
 // Project imports:
 import 'package:myafyahub/application/core/services/login_utils.dart';
 import 'package:myafyahub/application/redux/states/app_state.dart';
 import 'package:myafyahub/domain/core/value_objects/app_widget_keys.dart';
+import 'package:myafyahub/presentation/engagement/home/pages/home_page.dart';
 import '../../mocks.dart';
 import '../../test_helpers.dart';
 
@@ -44,5 +49,57 @@ void main() {
     await tester.pumpAndSettle();
 
     expect(find.byKey(feedbackBottomSheet), findsOneWidget);
+  });
+
+  testWidgets('should sign in user if valid', (WidgetTester tester) async {
+    final Store<AppState> store =
+        Store<AppState>(initialState: AppState.initial());
+
+    final MockShortSILGraphQlClient mockShortSILGraphQlClient =
+        MockShortSILGraphQlClient.withResponse(
+      'idToken',
+      'endpoint',
+      Response(
+        json.encode(<String, dynamic>{
+          'data': <String, dynamic>{
+            'login': mockLoginResponse,
+            'fetchRecentContent': contentMock,
+          }
+        }),
+        201,
+      ),
+    );
+
+    await mockNetworkImages(() async {
+      await buildTestWidget(
+        tester: tester,
+        store: store,
+        client: mockShortSILGraphQlClient,
+        widget: Material(
+          child: Builder(
+            builder: (BuildContext context) {
+              return RawMaterialButton(
+                onPressed: () => signInUser(
+                  context: context,
+                  phoneNumber: '0712345678',
+                  pin: '1111',
+                ),
+              );
+            },
+          ),
+        ),
+      );
+
+      await tester.pumpAndSettle();
+      await tester.tap(find.byType(RawMaterialButton));
+      await tester.pumpAndSettle();
+
+      expect(find.byKey(feedbackBottomSheet), findsNothing);
+      expect(
+        store.state.onboardingState!.phoneLogin!.phoneNumber,
+        '0712345678',
+      );
+      expect(find.byType(HomePage), findsOneWidget);
+    });
   });
 }

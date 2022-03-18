f677eb5272e9ea826505194394708df0a0748f90
Paulmburu <paulmburu53@gmail.com>
2022-03-18 10:08:14 +0300

    chore: bump security questions coverage

--- test/widgets_tests/redux/actions/onboarding/security_questions/verify_security_questions_action_test.dart
+++ test/widgets_tests/redux/actions/onboarding/security_questions/verify_security_questions_action_test.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
 
+import 'package:afya_moja_core/afya_moja_core.dart';
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:myafyahub/application/redux/actions/security_questions/verify_security_questions_action.dart';
+import 'package:myafyahub/application/redux/actions/update_onboarding_state_action.dart';
 import 'package:myafyahub/application/redux/states/app_state.dart';
+import 'package:myafyahub/domain/core/value_objects/enums.dart';
 
 import '../../../../../mocks.dart';
 
@@ -38,6 +41,46 @@ void main() {
           await storeTester.waitUntil(VerifySecurityQuestionAction);
 
       expect(info.state.onboardingState?.hasVerifiedSecurityQuestions, false);
+    });
+
+    test('verifies security questions successfully', () async {
+      storeTester.dispatch(
+        UpdateOnboardingStateAction(
+          securityQuestions: <SecurityQuestion>[
+            SecurityQuestion.initial(),
+            SecurityQuestion.initial(),
+            SecurityQuestion.initial(),
+          ],
+          securityQuestionsResponses: <SecurityQuestionResponse>[
+            SecurityQuestionResponse.initial(),
+            SecurityQuestionResponse.initial().copyWith(response: ''),
+            SecurityQuestionResponse.initial(),
+          ],
+          currentOnboardingStage: CurrentOnboardingStage.PINExpired,
+        ),
+      );
+      storeTester.dispatch(
+        VerifySecurityQuestionAction(
+          client: MockShortGraphQlClient.withResponse(
+            'idToken',
+            'endpoint',
+            Response(
+              json.encode(<String, dynamic>{
+                'data': <String, dynamic>{
+                  'verifySecurityQuestionResponses': true,
+                },
+              }),
+              201,
+            ),
+          ),
+          verifySecurityQuestionsEndpoint: '',
+        ),
+      );
+
+      final TestInfo<AppState> info =
+          await storeTester.waitUntil(VerifySecurityQuestionAction);
+
+      expect(info.state.onboardingState?.hasVerifiedSecurityQuestions, true);
     });
   });
 }

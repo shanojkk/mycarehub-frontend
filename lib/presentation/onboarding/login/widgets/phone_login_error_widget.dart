fbfbf0b57199322a87220976b23742ed19b6493f
Eugene <eugenemrlens@gmail.com>
2022-04-26 12:27:21 +0300

    fix: toggle invalid credential when resetting pin

--- lib/presentation/onboarding/login/widgets/phone_login_error_widget.dart
+++ lib/presentation/onboarding/login/widgets/phone_login_error_widget.dart
@@ -1,5 +1,8 @@
 import 'package:afya_moja_core/afya_moja_core.dart';
+import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
+import 'package:myafyahub/application/redux/actions/update_onboarding_state_action.dart';
+import 'package:myafyahub/application/redux/states/app_state.dart';
 import 'package:myafyahub/domain/core/value_objects/app_strings.dart';
 import 'package:myafyahub/domain/core/value_objects/app_widget_keys.dart';
 import 'package:myafyahub/presentation/router/routes.dart';
@@ -24,6 +27,10 @@ class PhoneLoginErrorWidget extends StatelessWidget {
             isFormValid &&
             phone != null &&
             phone != UNKNOWN) {
+          StoreProvider.dispatch<AppState>(
+            context,
+            UpdateOnboardingStateAction(invalidCredentials: false),
+          );
           Navigator.of(context).pushReplacementNamed(AppRoutes.forgotPINPage);
         }
       },

702baa833b59fc79ed3ae5e172f673d92cce4b1c
johnngugi <johnngugi777@gmail.com>
2022-05-06 15:23:00 +0300

    fixup! feat: pagination indicator

--- test/mocks.dart
+++ test/mocks.dart
@@ -2082,231 +2082,234 @@ Map<String, dynamic> mockViralLoadData = <String, dynamic>{
 };
 
 final Map<String, dynamic> mockHealthTimelineItems = <String, dynamic>{
-  'patientTimeline': <Map<String, dynamic>>[
-    <String, dynamic>{
-      'category': <Map<String, dynamic>>[
-        <String, dynamic>{
+  'patientHealthTimeline': <String, dynamic>{
+    'totalCount': 0,
+    'timeline': <Map<String, dynamic>>[
+      <String, dynamic>{
+        'category': <Map<String, dynamic>>[
+          <String, dynamic>{
+            'coding': <Map<String, dynamic>>[
+              <String, dynamic>{
+                'code': 'vital-signs',
+                'display': 'Vital Signs',
+                'system':
+                    'http://terminology.hl7.org/CodeSystem/observation-category'
+              }
+            ],
+            'text': 'Vital Signs'
+          }
+        ],
+        'code': <String, dynamic>{
           'coding': <Map<String, dynamic>>[
             <String, dynamic>{
-              'code': 'vital-signs',
-              'display': 'Vital Signs',
-              'system':
-                  'http://terminology.hl7.org/CodeSystem/observation-category'
+              'code': '5088',
+              'display': 'Temperature (c)',
+              'system': '/orgs/CIEL/sources/CIEL/concepts/5088/'
             }
           ],
-          'text': 'Vital Signs'
-        }
-      ],
-      'code': <String, dynamic>{
-        'coding': <Map<String, dynamic>>[
+          'text': 'Temperature (c)'
+        },
+        'effectiveDateTime': '2022-05-04',
+        'id': '9ce7577a-4975-4e45-b48f-67cea229be45',
+        'resourceType': 'Observation',
+        'status': 'preliminary',
+        'subject': <String, dynamic>{
+          'display': '8371c56f-2cc3-4ea9-9db7-2befef290f9c',
+          'reference': 'Patient/8371c56f-2cc3-4ea9-9db7-2befef290f9c'
+        },
+        'timelineDate': '2022-05-04',
+        'valueString': '36.5'
+      },
+      <String, dynamic>{
+        'category': <Map<String, dynamic>>[
           <String, dynamic>{
-            'code': '5088',
-            'display': 'Temperature (c)',
-            'system': '/orgs/CIEL/sources/CIEL/concepts/5088/'
+            'coding': <Map<String, dynamic>>[
+              <String, dynamic>{
+                'code': 'laboratory',
+                'display': 'Laboratory',
+                'system':
+                    'http://terminology.hl7.org/CodeSystem/observation-category'
+              }
+            ],
+            'text': 'Laboratory'
           }
         ],
-        'text': 'Temperature (c)'
-      },
-      'effectiveDateTime': '2022-05-04',
-      'id': '9ce7577a-4975-4e45-b48f-67cea229be45',
-      'resourceType': 'Observation',
-      'status': 'preliminary',
-      'subject': <String, dynamic>{
-        'display': '8371c56f-2cc3-4ea9-9db7-2befef290f9c',
-        'reference': 'Patient/8371c56f-2cc3-4ea9-9db7-2befef290f9c'
-      },
-      'timelineDate': '2022-05-04',
-      'valueString': '36.5'
-    },
-    <String, dynamic>{
-      'category': <Map<String, dynamic>>[
-        <String, dynamic>{
+        'code': <String, dynamic>{
           'coding': <Map<String, dynamic>>[
             <String, dynamic>{
-              'code': 'laboratory',
-              'display': 'Laboratory',
-              'system':
-                  'http://terminology.hl7.org/CodeSystem/observation-category'
+              'code': '306',
+              'display': 'Widal test',
+              'system': '/orgs/CIEL/sources/CIEL/concepts/306/'
             }
           ],
-          'text': 'Laboratory'
-        }
-      ],
-      'code': <String, dynamic>{
-        'coding': <Map<String, dynamic>>[
+          'text': 'Widal test'
+        },
+        'effectiveDateTime': '2022-03-05',
+        'id': '954aa3fc-0a9d-46e8-a145-703571ee434a',
+        'resourceType': 'Observation',
+        'status': 'preliminary',
+        'subject': <String, dynamic>{
+          'display': '8371c56f-2cc3-4ea9-9db7-2befef290f9c',
+          'reference': 'Patient/8371c56f-2cc3-4ea9-9db7-2befef290f9c'
+        },
+        'timelineDate': '2022-03-05',
+        'valueString': 'Non Reactive'
+      },
+      <String, dynamic>{
+        'category': <Map<String, dynamic>>[
           <String, dynamic>{
-            'code': '306',
-            'display': 'Widal test',
-            'system': '/orgs/CIEL/sources/CIEL/concepts/306/'
+            'coding': <Map<String, dynamic>>[
+              <String, dynamic>{
+                'code': 'laboratory',
+                'display': 'Laboratory',
+                'system':
+                    'http://terminology.hl7.org/CodeSystem/observation-category'
+              }
+            ],
+            'text': 'Laboratory'
           }
         ],
-        'text': 'Widal test'
-      },
-      'effectiveDateTime': '2022-03-05',
-      'id': '954aa3fc-0a9d-46e8-a145-703571ee434a',
-      'resourceType': 'Observation',
-      'status': 'preliminary',
-      'subject': <String, dynamic>{
-        'display': '8371c56f-2cc3-4ea9-9db7-2befef290f9c',
-        'reference': 'Patient/8371c56f-2cc3-4ea9-9db7-2befef290f9c'
-      },
-      'timelineDate': '2022-03-05',
-      'valueString': 'Non Reactive'
-    },
-    <String, dynamic>{
-      'category': <Map<String, dynamic>>[
-        <String, dynamic>{
+        'code': <String, dynamic>{
           'coding': <Map<String, dynamic>>[
             <String, dynamic>{
-              'code': 'laboratory',
-              'display': 'Laboratory',
-              'system':
-                  'http://terminology.hl7.org/CodeSystem/observation-category'
+              'code': '306',
+              'display': 'Widal test',
+              'system': '/orgs/CIEL/sources/CIEL/concepts/306/'
             }
           ],
-          'text': 'Laboratory'
-        }
-      ],
-      'code': <String, dynamic>{
-        'coding': <Map<String, dynamic>>[
-          <String, dynamic>{
-            'code': '306',
-            'display': 'Widal test',
-            'system': '/orgs/CIEL/sources/CIEL/concepts/306/'
-          }
-        ],
-        'text': 'Widal test'
+          'text': 'Widal test'
+        },
+        'effectiveDateTime': '2022-03-05',
+        'id': 'e22f4054-4f97-4996-b0aa-ffee9765e283',
+        'resourceType': 'Observation',
+        'status': 'preliminary',
+        'subject': <String, dynamic>{
+          'display': '8371c56f-2cc3-4ea9-9db7-2befef290f9c',
+          'reference': 'Patient/8371c56f-2cc3-4ea9-9db7-2befef290f9c'
+        },
+        'timelineDate': '2022-03-05',
+        'valueString': 'Non Reactive'
       },
-      'effectiveDateTime': '2022-03-05',
-      'id': 'e22f4054-4f97-4996-b0aa-ffee9765e283',
-      'resourceType': 'Observation',
-      'status': 'preliminary',
-      'subject': <String, dynamic>{
-        'display': '8371c56f-2cc3-4ea9-9db7-2befef290f9c',
-        'reference': 'Patient/8371c56f-2cc3-4ea9-9db7-2befef290f9c'
-      },
-      'timelineDate': '2022-03-05',
-      'valueString': 'Non Reactive'
-    },
-    <String, dynamic>{
-      'category': <String>['medication'],
-      'clinicalStatus': <String, dynamic>{
-        'coding': <Map<String, dynamic>>[
-          <String, dynamic>{
-            'code': '165586',
-            'display': "Milk powder (cow's milk)",
-            'system': '/orgs/CIEL/sources/CIEL/concepts/165586/'
-          }
-        ],
-        'text': "Milk powder (cow's milk)"
-      },
-      'code': <String, dynamic>{},
-      'id': '35fe4b5e-1f26-4d65-89bf-bdc22a4ac9c5',
-      'patient': <String, dynamic>{
-        'display': '8371c56f-2cc3-4ea9-9db7-2befef290f9c',
-        'reference': 'Patient/8371c56f-2cc3-4ea9-9db7-2befef290f9c'
-      },
-      'reaction': <Map<String, dynamic>>[
-        <String, dynamic>{
-          'description': "Milk powder (cow's milk)",
-          'manifestation': <Map<String, dynamic>>[
+      <String, dynamic>{
+        'category': <String>['medication'],
+        'clinicalStatus': <String, dynamic>{
+          'coding': <Map<String, dynamic>>[
             <String, dynamic>{
+              'code': '165586',
+              'display': "Milk powder (cow's milk)",
+              'system': '/orgs/CIEL/sources/CIEL/concepts/165586/'
+            }
+          ],
+          'text': "Milk powder (cow's milk)"
+        },
+        'code': <String, dynamic>{},
+        'id': '35fe4b5e-1f26-4d65-89bf-bdc22a4ac9c5',
+        'patient': <String, dynamic>{
+          'display': '8371c56f-2cc3-4ea9-9db7-2befef290f9c',
+          'reference': 'Patient/8371c56f-2cc3-4ea9-9db7-2befef290f9c'
+        },
+        'reaction': <Map<String, dynamic>>[
+          <String, dynamic>{
+            'description': "Milk powder (cow's milk)",
+            'manifestation': <Map<String, dynamic>>[
+              <String, dynamic>{
+                'coding': <Map<String, dynamic>>[
+                  <String, dynamic>{
+                    'code': '512',
+                    'display': 'Rash',
+                    'system': '/orgs/CIEL/sources/CIEL/concepts/165586/'
+                  }
+                ],
+                'text': 'Rash'
+              }
+            ],
+            'severity': 'severe',
+            'substance': <String, dynamic>{
               'coding': <Map<String, dynamic>>[
                 <String, dynamic>{
-                  'code': '512',
-                  'display': 'Rash',
+                  'code': '165586',
+                  'display': "Milk powder (cow's milk)",
                   'system': '/orgs/CIEL/sources/CIEL/concepts/165586/'
                 }
               ],
-              'text': 'Rash'
+              'text': "Milk powder (cow's milk)"
+            }
+          }
+        ],
+        'recordedDate': '2022-03-04',
+        'resourceType': 'AllergyIntolerance',
+        'timelineDate': '2022-03-04',
+        'type': 'allergy',
+        'verificationStatus': <String, dynamic>{}
+      },
+      <String, dynamic>{
+        'category': <String, dynamic>{
+          'coding': <Map<String, dynamic>>[
+            <String, dynamic>{
+              'code': '114100',
+              'display': 'Pneumonia',
+              'system': '/orgs/CIEL/sources/CIEL/concepts/114100/'
             }
           ],
-          'severity': 'severe',
-          'substance': <String, dynamic>{
-            'coding': <Map<String, dynamic>>[
+          'text': 'Pneumonia'
+        },
+        'effectiveDateTime': '2022-03-04',
+        'id': 'ab7313bb-3688-4f67-87db-e98bb69d0263',
+        'medicationCodeableConcept': <String, dynamic>{
+          'coding': <Map<String, dynamic>>[
+            <String, dynamic>{
+              'code': '71160',
+              'display': 'Amoxicillin',
+              'system': '/orgs/CIEL/sources/CIEL/concepts/71160/'
+            }
+          ],
+          'text': 'Amoxicillin'
+        },
+        'resourceType': 'MedicationStatement',
+        'status': 'unknown',
+        'subject': <String, dynamic>{
+          'display': '340611da-fcd0-4fc2-bb29-ef6ba3bbcb0b',
+          'reference': 'Patient/340611da-fcd0-4fc2-bb29-ef6ba3bbcb0b'
+        },
+        'timelineDate': '2022-03-04'
+      },
+      <String, dynamic>{
+        'category': <dynamic>[
+          <String, dynamic>{
+            'coding': <dynamic>[
               <String, dynamic>{
-                'code': '165586',
-                'display': "Milk powder (cow's milk)",
-                'system': '/orgs/CIEL/sources/CIEL/concepts/165586/'
+                'code': 'vital-signs',
+                'display': 'Vital Signs',
+                'system':
+                    'http://terminology.hl7.org/CodeSystem/observation-category'
               }
             ],
-            'text': "Milk powder (cow's milk)"
-          }
-        }
-      ],
-      'recordedDate': '2022-03-04',
-      'resourceType': 'AllergyIntolerance',
-      'timelineDate': '2022-03-04',
-      'type': 'allergy',
-      'verificationStatus': <String, dynamic>{}
-    },
-    <String, dynamic>{
-      'category': <String, dynamic>{
-        'coding': <Map<String, dynamic>>[
-          <String, dynamic>{
-            'code': '114100',
-            'display': 'Pneumonia',
-            'system': '/orgs/CIEL/sources/CIEL/concepts/114100/'
+            'text': 'Vital Signs'
           }
         ],
-        'text': 'Pneumonia'
-      },
-      'effectiveDateTime': '2022-03-04',
-      'id': 'ab7313bb-3688-4f67-87db-e98bb69d0263',
-      'medicationCodeableConcept': <String, dynamic>{
-        'coding': <Map<String, dynamic>>[
-          <String, dynamic>{
-            'code': '71160',
-            'display': 'Amoxicillin',
-            'system': '/orgs/CIEL/sources/CIEL/concepts/71160/'
-          }
-        ],
-        'text': 'Amoxicillin'
-      },
-      'resourceType': 'MedicationStatement',
-      'status': 'unknown',
-      'subject': <String, dynamic>{
-        'display': '340611da-fcd0-4fc2-bb29-ef6ba3bbcb0b',
-        'reference': 'Patient/340611da-fcd0-4fc2-bb29-ef6ba3bbcb0b'
-      },
-      'timelineDate': '2022-03-04'
-    },
-    <String, dynamic>{
-      'category': <dynamic>[
-        <String, dynamic>{
+        'code': <String, dynamic>{
           'coding': <dynamic>[
             <String, dynamic>{
-              'code': 'vital-signs',
-              'display': 'Vital Signs',
-              'system':
-                  'http://terminology.hl7.org/CodeSystem/observation-category'
+              'code': '5096',
+              'display': 'Return visit date',
+              'system': '/orgs/CIEL/sources/CIEL/concepts/5096/'
             }
           ],
-          'text': 'Vital Signs'
-        }
-      ],
-      'code': <String, dynamic>{
-        'coding': <dynamic>[
-          <String, dynamic>{
-            'code': '5096',
-            'display': 'Return visit date',
-            'system': '/orgs/CIEL/sources/CIEL/concepts/5096/'
-          }
-        ],
-        'text': 'Return visit date'
-      },
-      'effectiveInstant': '2022-04-05T09:36:28Z',
-      'id': '3f753b78-7c91-4693-b8fd-f45f5a6538e6',
-      'resourceType': 'Observation',
-      'status': 'preliminary',
-      'subject': <String, dynamic>{
-        'display': '510a98f5-8e39-465f-abf8-663c150ce355',
-        'reference': 'Patient/510a98f5-8e39-465f-abf8-663c150ce355'
-      },
-      'timelineDate': '2022-04-05T09:36:28Z',
-      'valueString': '2022-04-21'
-    }
-  ]
+          'text': 'Return visit date'
+        },
+        'effectiveInstant': '2022-04-05T09:36:28Z',
+        'id': '3f753b78-7c91-4693-b8fd-f45f5a6538e6',
+        'resourceType': 'Observation',
+        'status': 'preliminary',
+        'subject': <String, dynamic>{
+          'display': '510a98f5-8e39-465f-abf8-663c150ce355',
+          'reference': 'Patient/510a98f5-8e39-465f-abf8-663c150ce355'
+        },
+        'timelineDate': '2022-04-05T09:36:28Z',
+        'valueString': '2022-04-21'
+      }
+    ],
+  },
 };
 
 final Map<String, dynamic> groupStateMock = <String, dynamic>{

f5caa9f713a7858ba15f1a0f195843a97ecf654e
Byron Kimani <byronkimani@Byrons-MacBook-Pro.local>
2022-09-01 09:36:42 +0300

    chore: opt out fixes
    - correct text on opt out section
    
    Signed-off-by: Byron Kimani <byronkimani@Byrons-MacBook-Pro.local>

--- lib/domain/core/value_objects/app_strings.dart
+++ lib/domain/core/value_objects/app_strings.dart
@@ -6,6 +6,7 @@ import 'package:flutter/widgets.dart';
 import 'package:pro_health_360/application/core/services/utils.dart';
 import 'package:pro_health_360/application/redux/states/app_state.dart';
 
+const String adminEmail = 'feedback@prohealth360.org';
 const String requestTimeoutError =
     'We could not perform this process in time. Please try again';
 
@@ -842,14 +843,14 @@ const String optOutOfMyCareHubTitleString = 'Opt out of UoNAfyaApp360?';
 const String youAreChoosingToOptOut =
     'You are choosing to opt out of UoNAfyaApp360. The following will happen to your account:';
 const String yourProfileWillBeDeleted =
-    '1. Your profile will be deleted. This action is cannot be undone.';
+    '1. Your profile will be deleted. This action cannot be undone.';
 const String youWillNotLogin =
     '2. You will not be able to continue to benefit from all the services on UoNAfyaApp360.';
 const String youWillNeedToRegisterString =
     '3. You will need to be registered again to access UoNAfyaApp360 services.';
 
 const String yourHealthRecordWillBeAnonymizedString =
-    '4. Your health record will be anonymized. If you wish to delete your data from UoNAfyaApp360, kindly contact the admin on ';
+    '4. Your health record will be anonymized. If you wish to delete your data from UoNAfyaApp360, kindly contact the admin on $adminEmail';
 const String areYouStillSureString =
     'Are you still sure that you want to opt out of UoNAfyaApp360?';
 
